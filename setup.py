669dbf0d6493318415ac314e71294e7df8f59b15
yy96 <chenyy1996@yahoo.com>
2021-09-18 17:24:18 +0800

    add basic file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='repository for module CS5228 project',
+    author='team_MARS',
+    license='',
+)
